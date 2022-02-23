3c830ac4b791c033a12c285e867e947e8984350e
takurou.kachi <tkoika89@gmail.com>
2022-02-24 06:01:36 +0900

    バリデーションを追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -45,6 +48,20 @@ public class UpdateServlet extends HttpServlet {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             t.setUpdated_at(currentTime);
 
+            // バリデーション
+            List<String> errors = TaskValidator.validate(t);
+            if(errors.size() > 0) {
+                em.close();
+
+                // エラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", t);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+                rd.forward(request, response);
+            } else {
+
             //データベースの更新
             em.getTransaction().begin();
             em.getTransaction().commit();
@@ -56,6 +73,8 @@ public class UpdateServlet extends HttpServlet {
 
             //indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
+
+            }
         }
     }
 
